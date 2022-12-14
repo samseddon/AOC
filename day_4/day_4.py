eb7f822ce1f58f2ea70cbbfe8077d3f287915292
sseddon <samuel.seddon@gmail.com>
2022-12-14 17:46:02 +0100

    on the way

---
+++ day_4/day_4.py
@@ -0,0 +1,8 @@
+file = open('input_day_4.txt', 'r')                                            
+Lines = file.readlines()  
+
+for line in Lines: 
+    elf_both = line.strip().split(',')
+    elf_1 = elf_
+    print(elf_1)
+    
