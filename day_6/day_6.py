68e2430f046c37477356a90f769f68a630900761
sseddon <samuel.seddon@gmail.com>
2022-12-17 13:35:35 +0100

    day_9 done

--- day_6/day_6.py
+++ day_6/day_6.py
@@ -1,9 +1,10 @@
 file = open('input_day_6.txt', 'r')                                            
 Line = file.readlines()
 signal = Line[0].strip()
-length = 14
-for i in range(length,len(signal)):
-    datastream = list(signal[i-length:i])
+length_signal_search = 14
+
+for i in range(length_signal_search,len(signal)):
+    datastream = list(signal[i-length_signal_search:i])
     datastream_reduced = list(set(datastream))
     datastream_reduced.sort()
     datastream.sort()
