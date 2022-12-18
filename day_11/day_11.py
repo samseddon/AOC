18e3dd0ee539c82ffe40e3a68d9d5843ac57622a
sseddon <samuel.seddon@gmail.com>
2022-12-18 16:43:10 +0100

    think it works but is very low

--- day_11/day_11.py
+++ day_11/day_11.py
@@ -1,14 +1,16 @@
+import math
+
 class Monkey:
     def __init__(self, starting_items, operation, test, throw_1,throw_2):
-        self.starting_items = starting_items
+        self.items = starting_items
         self.operation_func = operation[0]
         self.operation_val  = operation[1]
         self.test           = test
         self.true_throw     = throw_1
         self.false_throw    = throw_2
+        self.inspected      = 0
 
-
-    def Operation(self, worry):
+    def operation(self, worry):
         if self.operation_val == 'old':
             easy_op = False
         else: 
@@ -22,15 +24,58 @@ class Monkey:
         if easy_op == False:
             return worry * worry 
 
+    def test_worry(self, worry):
+        if worry % self.test == 0:
+            return(True)
+
+        else:
+            return(False)
+
+
+    def where_throw_item(self, worry, bool):
+        if bool == True:
+            return self.true_throw
+
+        else:
+            return self.false_throw
+    
+    def inspect(self):
+        self.inspected += 1
+
+    def throw(self):
+        new_items = self.items[1:]
+        self.items = new_items
+
 file = open('input_day_10.txt', 'r')
-file = open('test_input.txt')
+#file = open('test_input.txt')
 Lines = file.readlines()  
-monkey = []
+Monkeys= []
 
 for i in range(len(Lines)):
     if Lines[i][:6] == 'Monkey':
-        monkey.append(Monkey(Lines[i+1].strip().split(' ')[2:],
+        items_unsplit = Lines[i+1].strip().split(' ')[2:]
+        items_split = []
+        for item in items_unsplit:
+            item = item.split(',')
+            items_split.append(int(item[0]))
+        Monkeys.append(Monkey(items_split,
                              Lines[i+2].strip().split(' ')[-2:],
-                             Lines[i+3].strip().split(' ')[-1],
-                             Lines[i+4].strip().split(' ')[-1],
-                             Lines[i+5].strip().split(' ')[-1]))
+                             int(Lines[i+3].strip().split(' ')[-1]),
+                             int(Lines[i+4].strip().split(' ')[-1]),
+                             int(Lines[i+5].strip().split(' ')[-1])))
+
+for _ in range(10000):
+    for monkey in Monkeys:
+        for item in monkey.items:
+            monkey.inspect()
+            worry = monkey.operation(item)
+            #worry = math.floor(worry/3)
+            next_monkey = monkey.where_throw_item(worry,monkey.test_worry(worry)) 
+            monkey.throw()
+            Monkeys[next_monkey].items.append(worry)
+    
+monkey_buisness = []
+for i in range(len(Monkeys)):
+    monkey_buisness.append(Monkeys[i].inspected)
+monkey_buisness = sorted(monkey_buisness)
+print(monkey_buisness[-1]*monkey_buisness[-2])
